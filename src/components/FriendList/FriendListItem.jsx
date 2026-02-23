ed308f6050d8a944c31c1fcd2a396680535a60e6
s-Ira-d <20irinka12@gmail.com>
2026-02-23 22:23:49 +0200

    .

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -5,7 +5,7 @@ const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
       <span className={`${css.status} ${isOnline ? css.online : css.offline}`}>
-        {isOnline ? "Online" : "Offline"}
+        {}
       </span>
 
       <img className={css.avatar} src={avatar} alt={`${name} avatar`} />
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,14 +1,15 @@
-import React from "react";
 import PropTypes from "prop-types";
+import css from "./Statistics.module.css";
 
 const Statistics = ({ title, stats }) => {
   return (
-    <section>
-      {title && <h2>{title}</h2>}
-      <ul>
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.statList}>
         {stats.map((item) => (
-          <li key={item.id}>
-            {item.label}: {item.percentage}%
+          <li key={item.id} className={css.item}>
+            <span className={css.label}>{item.label}</span>
+            <span className={css.percentage}>{item.percentage}%</span>
           </li>
         ))}
       </ul>
